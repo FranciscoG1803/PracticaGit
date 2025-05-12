71fef76d1368bfb4b20d64ca7eea311c846d9d87
FranciscoG1803 <00077225@uca.edu.sv>
2025-05-11 21:53:46 -0600

    Primer commit

---
+++ prac1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int N;
+
+    cout << "Ingrese un numero: ";
+    cin >> N;
+
+    for (int i = 0; i <= N; i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
